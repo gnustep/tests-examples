f75178f77113e7161cbc466b886a46a78dbdf071
Richard Frith-MacDonald <rfm@gnu.org>
1999-07-18 16:40:42 +0000

    Tidied
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@4616 72102866-910b-0410-8b05-ffd578937521

--- gui/matrix.m
+++ gui/matrix.m
@@ -8,15 +8,15 @@
   
    This file is part of the GNUstep GUI Testing package
    
-   This library is free software; you can redistribute it and/or
+   This file is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public
    License as published by the Free Software Foundation; either
    version 2 of the License, or (at your option) any later version.
    
-   This library is distributed in the hope that it will be useful,
+   This file is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   General Public License for more details.
    
    You should have received a copy of the GNU General Public
    License along with this library; if not, write to the Free
