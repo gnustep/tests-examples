2b28412dd93dbd8a6fce3fbc986bee8ded2ef3cd
Nicola Pero <n.pero@mi.flashnet.it>
2000-02-05 05:13:21 +0000

    Removed Info menu in favour of a single Info... item; added Hide menu item
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/branches/freeze-0_6_5@5932 72102866-910b-0410-8b05-ffd578937521

--- gui/Calculator/main.m
+++ gui/Calculator/main.m
@@ -56,20 +56,13 @@ main (void)
   pool = [NSAutoreleasePool new];
   initialize_gnustep_backend ();
   app = [NSApplication sharedApplication];
-  [app setApplicationIconImage: [NSImage imageNamed: @"Calculator.app.tiff"]];
+  [app setApplicationIconImage: [NSImage imageNamed: 
+					   @"Calculator.app.tiff"]];
   mainMenu = [NSMenu new];
-  // Info SubMenu
-  menuItem = [mainMenu addItemWithTitle: @"Info" 
-		       action: NULL 
-		       keyEquivalent: @""];
-  menu = [NSMenu new];
-  [mainMenu setSubmenu: menu forItem: menuItem];
-  [menu addItemWithTitle: @"Info Panel..." 
-	action: @selector (runInfoPanel:) 
-	keyEquivalent: @""];
-  [menu addItemWithTitle: @"Help..." 
-	action: @selector (orderFrontHelpPanel:)
-	keyEquivalent: @"?"];
+  // Info
+  [mainMenu addItemWithTitle: @"Info..." 
+	    action: @selector (runInfoPanel:) 
+	    keyEquivalent: @""];
   // Edit SubMenu
   menuItem = [mainMenu addItemWithTitle: @"Edit" 	
 		       action: NULL 
@@ -92,7 +85,10 @@ main (void)
   [menu addItemWithTitle: @"SelectAll" 
 	action: @selector (selectAll:) 
 	keyEquivalent: @"a"];
-  
+
+  [mainMenu addItemWithTitle: @"Hide" 
+	action: @selector (hide:) 
+	keyEquivalent: @"h"];  
   [mainMenu addItemWithTitle: @"Quit" 
 	    action: @selector (terminate:)
 	    keyEquivalent: @"q"];	
