2597061e226b85a674ed816c30ae5155084a22fb
Nicola Pero <n.pero@mi.flashnet.it>
2000-02-13 00:41:59 +0000

    Improved autoresizing
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/branches/freeze-0_6_5@6000 72102866-910b-0410-8b05-ffd578937521

--- gui/GSTest/StringDrawing-test/StringDrawing-test.m
+++ gui/GSTest/StringDrawing-test/StringDrawing-test.m
@@ -96,12 +96,12 @@
   GSVbox *vbox;
   NSButton *button;
   NSScrollView *scrollView;
-  NSRect winFrame;
 
   text = [TrivialTextView new];
 
+  /* Minimum size we want it to have */
   scrollView = [[NSScrollView alloc] 
-		 initWithFrame: NSMakeRect (0, 0, 500, 222)];
+		 initWithFrame: NSMakeRect (0, 0, 60, 60)];
   [scrollView setDocumentView: text];
   [text release];
   [scrollView setHasHorizontalScroller: YES];
@@ -126,11 +126,9 @@
   [vbox addView: button
 	enablingYResizing: NO];
   [vbox addView: scrollView];
+  [vbox setAutoresizingMask: NSViewWidthSizable | NSViewHeightSizable];
   
-  winFrame.size = [vbox frame].size;
-  winFrame.origin = NSMakePoint (100, 200);
-  
-  win = [[NSWindow alloc] initWithContentRect: winFrame
+  win = [[NSWindow alloc] initWithContentRect: NSMakeRect (100, 100, 500, 250)
 			  styleMask: (NSTitledWindowMask 
 				      | NSClosableWindowMask 
 				      | NSMiniaturizableWindowMask 
