bac8a509fa71a415376d82f9ca098c8dfd8f292a
Nicola Pero <n.pero@mi.flashnet.it>
2001-01-09 00:29:24 +0000

    moved here
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@8525 72102866-910b-0410-8b05-ffd578937521

---
+++ gui/Old/Edit/Document.h
@@ -0,0 +1,133 @@
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#include <AppKit/NSDocument.h>
+
+@class NSTextView;
+@class NSTextStorage;
+@class NSPrintInfo;
+
+/* These get added to the string encodings so we have a common language to refer to file types */
+enum {
+    UnknownStringEncoding = -1000,
+    RichTextStringEncoding = -1001,
+    RichTextWithGraphicsStringEncoding = -1002
+};
+
+/* List of encodings to display in the open/save panels */
+extern const int *SupportedEncodings (void);
+
+/* Set up an encoding popup with the specified parameters. If the popup contains 1 item, it's also initialized with the supported encodings. */
+extern void SetUpEncodingPopupButton(NSPopUpButton *popup, int selectedEncoding, BOOL includeDefaultItem);
+
+@interface Document : NSDocument {
+    NSTextStorage *textStorage;
+    NSString *documentName;		/* If nil, never saved */
+    NSScrollView *scrollView;		/* ScrollView containing document */
+    NSPrintInfo *printInfo;		/* PrintInfo, used when hasMultiplePages is true */
+    BOOL isDocumentEdited;
+    BOOL hasMultiplePages;
+    BOOL isRichText;
+    BOOL uniqueZone;			/* YES if the zone was created specially for this document */
+    int encodingIfPlainText;
+    NSString *potentialSaveDirectory;	/* if non-nil, is path prefix where to save it. */
+}
+
+/* Don't call init; call one of these methods... */
+- (id)initWithPath:(NSString *)filename encoding:(int)encoding uniqueZone:(BOOL)flag;	/* Should be an absolute path here; nil for untitled. uniqueZone = YES indicates the zone should be recycled when the doc is dealloced. */
++ (BOOL)openDocumentWithPath:(NSString *)filename encoding:(int)encoding;	/* Brings window front. Checks to see if document already open. */
++ (BOOL)openUntitled;	/* Brings window front */
+
+/* These set/get the documentName instance var and also set the window title accordingly. "nil" is used if no title. */
+- (void)setDocumentName:(NSString *)fileName;
+- (NSString *)documentName;
+
+/* These determine if document has been edited since last save */
+- (void)setDocumentEdited:(BOOL)flag;
+- (BOOL)isDocumentEdited;
+
+/* Is the document rich? */
+- (BOOL)isRichText;
+- (void)setRichText:(BOOL)flag;
+
+/* Hyphenation factor (0.0-1.0, 0.0 == disabled) */
+- (float)hyphenationFactor;
+- (void)setHyphenationFactor:(float)factor;
+
+/* View size (as it should be saved in a RTF file) */
+- (NSSize)viewSize;
+- (void)setViewSize:(NSSize)size;
+
+/* Attributes */
+- (NSTextStorage *)textStorage;
+- (NSText *)firstTextView;
+- (NSWindow *)window;
+//- (NSLayoutManager *)layoutManager;
+
+/* Misc methods */
++ (Document *)documentForWindow:(NSWindow *)window;
++ (Document *)documentForPath:(NSString *)filename;
++ (NSString *)cleanedUpPath:(NSString *)filename;
++ (NSView *)encodingAccessory:(int)defaultEncoding includeDefaultEntry:(BOOL)includeDefaultItem;
++ (unsigned)numberOfOpenDocuments;
+- (void)doForegroundLayoutToCharacterIndex:(unsigned)loc;
+
+/* Page-oriented methods */
+- (void)addPage;
+- (void)removePage;
+- (unsigned)numberOfPages;
+- (void)setHasMultiplePages:(BOOL)flag;
+- (BOOL)hasMultiplePages;
+- (void)setPrintInfo:(NSPrintInfo *)anObject;
+- (NSPrintInfo *)printInfo;
+
+/* Printing a document */
+- (void)printDocumentUsingPrintPanel:(BOOL)uiFlag;
+
+/* Saving helpers. These return NO or nil if user cancels the save... */
+- (BOOL)getDocumentName:(NSString **)newName encoding:(int *)encodingForSaving oldName:(NSString *)oldName oldEncoding:(int)encoding;
+- (BOOL)saveDocument:(BOOL)showSavePanel;	/* Saves under documentName; if not set, tries to set it first */
+- (BOOL)canCloseDocument;	/* Assures document is saved or user doesn't care about the changes; returns NO if user cancels */
++ (void)openWithEncodingAccessory:(BOOL)flag;
+
+/* Outlet methods */
++ (void)setEncodingPopupButton:(id)anObject;	/* In the save panel... */
++ (void)setEncodingAccessory:(id)anObject;	/* In the save panel... */
+
+/* Action methods */
++ (void)open:(id)sender;
+- (void)saveAs:(id)sender;
+- (void)saveTo:(id)sender;
+- (void)save:(id)sender;
+- (void)revert:(id)sender;
+- (void)close:(id)sender;
+- (void)runPageLayout:(id)sender;
+- (void)orderFrontFindPanel:(id)sender;
+- (void)findNext:(id)sender;
+- (void)findPrevious:(id)sender;
+- (void)enterSelection:(id)sender;
+- (void)jumpToSelection:(id)sender;
+- (void)toggleRich:(id)sender;
+- (void)togglePageBreaks:(id)sender;
+- (void)printDocument:(id)sender;  /* action cover for [self printDocumentUsingPrintPanel:YES] */
+
+/* When the preference "OpenPanelFollowsMainWindow" is set to YES, this is used to save/get the last used directory for the save/open panel.
+*/
++ (void)setLastOpenSavePanelDirectory:(NSString *)dir;
++ (NSString *)openSavePanelDirectory;
+
+/* setPotentialSaveDirectory gets called automatically when a doc is made "new" UNTITLED.  The name is taken from the current main window, if any.  The directory is used to put up the save panel the first time the doc is saved from the UNTITLED state.  It is only used when the preference "OpenPanelFollowsMainWindow" is set to YES.
+*/
+- (void)setPotentialSaveDirectory:(NSString *)nm;
+- (NSString *)potentialSaveDirectory;
+
+
+
+@end
+
+@interface Document (ReadWrite)
+
+/* File loading. Returns NO if not successful. Doesn't set documentName. */
+- (BOOL)loadFromPath:(NSString *)fileName encoding:(int)encoding;	/* If Unknown, tries to guess */
+- (BOOL)saveToPath:(NSString *)fileName encoding:(int)encoding updateFilenames:(BOOL)updateFIleNamesFlag;
+
+@end
---
+++ gui/Old/Edit/Edit.m
@@ -0,0 +1,329 @@
+/*
+   Edit.m
+   
+   Copyright (C) 1996 Free Software Foundation, Inc.
+   
+   Author:  Felipe A. Rodriguez <far@ix.netcom.com>
+   Date: July 1998
+   
+   This file is part of the GNUstep GUI X/RAW Library.
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+   
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+   
+   You should have received a copy of the GNU Library General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02111, USA.
+*/
+
+#import <Foundation/NSAutoreleasePool.h>
+#import <AppKit/AppKit.h>
+#import "Controller.h"
+#import "Document.h"
+
+void createMenu();
+
+
+int
+main(int argc, char **argv, char** env)
+{
+id pool = [NSAutoreleasePool new];
+NSApplication *theApp;
+
+#if LIB_FOUNDATION_LIBRARY
+  	[NSProcessInfo initializeWithArguments: argv count: argc environment: env];
+#endif
+
+//[NSObject enableDoubleReleaseCheck: YES];
+	theApp = [NSApplication sharedApplication];
+	[theApp setDelegate: [[Controller alloc] init]];
+	createMenu();									// create a generic menu
+	[theApp run];
+
+	[pool release];
+	
+	return 0;
+}
+//*****************************************************************************
+//
+// 		create interface modeller's menu 
+//
+//*****************************************************************************
+
+void createMenu()
+{
+  NSMenu* menu;
+  NSMenu* info;
+  NSMenu* file;
+  NSMenu* format;
+  NSMenu* base;
+  NSMenu* text;
+  NSMenu* edit;
+  NSMenu* util;
+  NSMenu* services;
+  NSMenu* windows;
+  SEL action = @selector(method:);
+
+  /*
+   *	Create the app menu
+   */
+  //menu = [[NSMenu alloc] initWithTitle: @"Edit"];
+  menu = [NSMenu new];
+
+  [menu addItemWithTitle: @"Info"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"File"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Edit"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Format"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Text"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Utilities"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Windows"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Print"
+		  action: action
+	   keyEquivalent: @"p"];
+
+  [menu addItemWithTitle: @"Services"
+		  action: action
+	   keyEquivalent: @""];
+
+  [menu addItemWithTitle: @"Hide"
+		  action: @selector(hide:)
+	   keyEquivalent: @"h"];
+
+  [menu addItemWithTitle: @"Quit"
+		  action: @selector(terminate:)
+	   keyEquivalent: @"q"];
+
+  /*
+   * Create the info submenu
+   */
+  info = [NSMenu new];
+  [menu setSubmenu: info
+	   forItem: [menu itemWithTitle: @"Info"]];
+
+  [info addItemWithTitle: @"Info Panel..."
+	          action: @selector(orderFrontStandardInfoPanel:)
+	   keyEquivalent: @""];
+
+  [info addItemWithTitle: @"Preferences..."
+		  action: action
+	   keyEquivalent: @""];
+
+  [info addItemWithTitle: @"Help"
+		  action: action
+	   keyEquivalent: @"?"];
+
+  /*
+   * Create the file submenu
+   */
+  file = [NSMenu new];
+  [menu setSubmenu: file
+	   forItem: [menu itemWithTitle: @"File"]];
+
+  [file addItemWithTitle: @"Open..."
+		  action: @selector(openDocument:)
+//		  action: @selector(open:)
+	   keyEquivalent: @"o"];
+
+  [file addItemWithTitle: @"New Application"
+		  action: action 
+	   keyEquivalent: @"n"];
+
+  [file addItemWithTitle: @"New Module"
+		  action: action
+	   keyEquivalent: @""];
+
+  [file addItemWithTitle: @"Save"
+	          action: @selector(saveTo:)
+	   keyEquivalent: @"s"];
+
+  [file addItemWithTitle: @"Save As..."
+	          action: @selector(saveAs:)
+	   keyEquivalent: @"S"];
+
+  [file addItemWithTitle: @"Save All"
+	action: @selector(saveAll:)
+	   keyEquivalent: @""];
+
+  [file addItemWithTitle: @"Revert to Saved"
+		  action: action 
+	   keyEquivalent: @"u"];
+
+  [file addItemWithTitle: @"Test Interface"
+		  action: action
+	   keyEquivalent: @"O"];
+
+  [file addItemWithTitle: @"Close"
+		  action: action
+	   keyEquivalent: @""];
+
+
+  /*
+   * Create the edit submenu
+   */
+  edit = [NSMenu new];
+  [menu setSubmenu: edit
+	   forItem: [menu itemWithTitle: @"Edit"]];
+
+  [edit addItemWithTitle: @"Cut"
+	          action: @selector(cut:)
+	   keyEquivalent: @"x"];
+
+  [edit addItemWithTitle: @"Copy"
+	          action: @selector(copy:)
+	   keyEquivalent: @"c"];
+
+  [edit addItemWithTitle: @"Paste"
+		  action: @selector(paste:)
+	   keyEquivalent: @"v"];
+
+  [edit addItemWithTitle: @"Delete"
+   	          action: @selector(delete:)
+	   keyEquivalent: @""];
+
+  [edit addItemWithTitle: @"Undelete"
+		  action: NULL
+	   keyEquivalent: @""];
+
+  [edit addItemWithTitle: @"Select All"
+  	          action: @selector(selectAll:)
+	   keyEquivalent: @"a"];
+
+  /*
+   * Create the format submenu
+   */
+  format = [[NSFontManager sharedFontManager] fontMenu: YES];
+  [menu setSubmenu: format
+	   forItem: [menu itemWithTitle: @"Format"]];
+
+  [format addItemWithTitle: @"Underline"
+  	          action: @selector(underline:)
+	   keyEquivalent: @""];
+  [format addItemWithTitle: @"Baseline"
+  	          action: NULL
+	   keyEquivalent: @""];
+  base = [NSMenu new];
+  [format setSubmenu: base
+	   forItem: [format itemWithTitle: @"Baseline"]];
+  [base addItemWithTitle: @"Superscript"
+  	          action: @selector(superscript:)
+	   keyEquivalent: @""];
+  [base addItemWithTitle: @"Subscript"
+  	          action: @selector(subscript:)
+	   keyEquivalent: @""];
+  [base addItemWithTitle: @"Unscript"
+  	          action: @selector(unscript:)
+	   keyEquivalent: @""];
+  [format addItemWithTitle: @"Copy Font" 
+	  action: @selector(copyFont:)
+	  keyEquivalent: @"3"];
+  [format addItemWithTitle: @"Paste Font" 
+	  action: @selector(pasteFont:) 
+	  keyEquivalent: @"4"];
+
+  /*
+   * Create the text submenu
+   */
+  text = [NSMenu new];
+  [menu setSubmenu: text
+	   forItem: [menu itemWithTitle: @"Text"]];
+
+  [text addItemWithTitle: @"Align Left"
+		  action: @selector(alignLeft:)
+	   keyEquivalent: @"{"];
+  [text addItemWithTitle: @"Center"
+		  action: @selector(alignCenter:)
+	   keyEquivalent: @"-"];
+  [text addItemWithTitle: @"Align Right"
+		  action: @selector(alignRight:)
+	   keyEquivalent: @"}"];
+  [text addItemWithTitle: @"Show Ruler" 
+	action: @selector(toggleRuler:) 
+	keyEquivalent: @""];
+  [text addItemWithTitle: @"Copy Ruler" 
+	action: @selector(copyRuler:) 
+	keyEquivalent: @"1"];
+  [text addItemWithTitle: @"Paste Ruler" 
+	action: @selector(pasteRuler:) 
+	keyEquivalent: @"2"];
+
+  /*
+   * Create the edit submenu
+   */
+  util = [NSMenu new];
+  [menu setSubmenu: util
+	   forItem: [menu itemWithTitle: @"Utilities"]];
+
+  [util addItemWithTitle: @"Inspector"
+		  action: @selector(showInspectorPanel:)
+	   keyEquivalent: @""];
+
+  [util addItemWithTitle: @"Palettes"
+		  action: @selector(showPalettePanel:) 
+	   keyEquivalent: @""];
+
+  [util addItemWithTitle: @"Load Palette"
+		  action: action
+	   keyEquivalent: @""];
+
+  /*
+   * Create the windows submenu
+   */
+  windows = [NSMenu new];
+  [menu setSubmenu: windows
+	   forItem: [menu itemWithTitle: @"Windows"]];
+
+  [windows addItemWithTitle: @"Arrange"
+		     action: @selector(arrangeInFront:)
+	      keyEquivalent: @""];
+
+  [windows addItemWithTitle: @"Miniaturize"
+		     action: @selector(performMiniaturize:)
+	      keyEquivalent: @"m"];
+
+  [windows addItemWithTitle: @"Close"
+		     action: @selector(performClose:)
+	      keyEquivalent: @"w"];
+
+  /*
+   * Create the windows submenu
+   */
+  services = [NSMenu new];
+  [menu setSubmenu: services
+	   forItem: [menu itemWithTitle: @"Services"]];
+
+  [[NSApplication sharedApplication] setMainMenu: menu];
+
+  [[NSApplication sharedApplication] setServicesMenu: services];
+
+  NSLog (@"start displaying the menu...");
+  [menu update];
+  [menu display];
+}
