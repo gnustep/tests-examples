a6cc90a4ca81eb496887be41a5c5c0a52e5f7dd6
Fred Kiefer <fredkiefer@gmx.de>
2009-05-20 21:48:46 +0000

    Add example theme by Fred Morcos <fred.morcos@gmail.com>
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@28290 72102866-910b-0410-8b05-ffd578937521

---
+++ gui/ExampleTheme/Rhea/Rhea.m
@@ -0,0 +1,22 @@
+#import "Rhea.h"
+
+#import <AppKit/AppKit.h>
+
+@implementation Rhea
+
+- (void) drawButton: (NSRect) frame 
+				 in: (NSCell*) cell 
+			   view: (NSView*) view 
+			  style: (int) style 
+			  state: (GSThemeControlState) state {
+	if (state == GSThemeNormalState)
+		[[NSColor blackColor] set];
+	else if (state == GSThemeHighlightedState)
+		[[NSColor whiteColor] set];
+	else if (state == GSThemeSelectedState)
+		[[NSColor redColor] set];
+	[[NSBezierPath bezierPathWithRect: frame] stroke];
+}
+
+@end
+
