2468afeba9ce81345c93b0a6209a8fe798b1f925
Eric Wasylishen <ewasylishen@gmail.com>
2011-04-29 22:58:25 +0000

    * NSTextView-test/NSTextView-test.m: Test multiple layout managers per text storage, and multiple text views per layout manager.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@32951 72102866-910b-0410-8b05-ffd578937521

--- gui/GSTest/NSTextView-test/NSTextView-test.m
+++ gui/GSTest/NSTextView-test/NSTextView-test.m
@@ -27,6 +27,7 @@
 @interface NSTextViewTest: NSObject <GSTest>
 {
   NSWindow *win;
+  NSTextStorage *storage;
 }
 -(void) restart;
 @end
@@ -35,50 +36,80 @@
 
 -(id) init
 {
-  NSBox *externalBox;
-  NSScrollView *scrollView;
-  NSTextView *textView;
+  NSView *container;
   NSRect winFrame;
-  NSRect textRect;
+  NSTextField *label;
+  
+  container = [NSView new];
 
-  scrollView = [[NSScrollView alloc] 
-		 initWithFrame: NSMakeRect (0, 0, 400, 300)];
-  [scrollView setHasHorizontalScroller: YES];
-  [scrollView setHasVerticalScroller: YES];
-  [scrollView setBorderType: NSBezelBorder];
-  [scrollView setAutoresizingMask: (NSViewWidthSizable | NSViewHeightSizable)];
+  {
+    NSTextContainer *tc1, *tc2, *tc3, *tc4, *tc5, *tc6;	
+    NSTextView *tv1, *tv2, *tv3, *tv4, *tv5, *tv6;
+    NSLayoutManager *lm1, *lm2;
+   
+    storage = [[NSTextStorage alloc] init];
+ 
+    lm1 = [[NSLayoutManager alloc] init];
+    lm2 = [[NSLayoutManager alloc] init];
+    [storage addLayoutManager: lm1];
+    [storage addLayoutManager: lm2];
+    [lm1 release];
+    [lm2 release];
 
-  textRect = [[scrollView contentView] frame];
-  textView = [[NSTextView alloc] initWithFrame: textRect];
-  [textView setBackgroundColor: [NSColor whiteColor]];
-  [textView setRichText: YES];
-  [textView setUsesFontPanel: YES];
-  [textView setDelegate: self];
-  [textView setHorizontallyResizable: NO];
-  [textView setVerticallyResizable: YES];
-  [textView setMinSize: NSMakeSize (0, 0)];
-  [textView setMaxSize: NSMakeSize (1E7, 1E7)];
-  [textView setAutoresizingMask: NSViewHeightSizable | NSViewWidthSizable];
-  [[textView textContainer] setContainerSize: NSMakeSize (textRect.size.width,
-							  1e7)];
-  [[textView textContainer] setWidthTracksTextView: YES];
+    tc1 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 150)];
+    tc2 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 150)];
+    tc3 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 150)];
+    tc4 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 150)];
+    tc5 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 300)];		
+    tc6 = [[NSTextContainer alloc] initWithContainerSize: NSMakeSize(150, 300)];
+    
+    [lm1 addTextContainer: tc1];
+    [lm1 addTextContainer: tc2];
+    [lm1 addTextContainer: tc3];
+    [lm1 addTextContainer: tc4];
+    [lm2 addTextContainer: tc5];
+    [lm2 addTextContainer: tc6];
+    [tc1 release];
+    [tc2 release];
+    [tc3 release];
+    [tc4 release];
+    [tc5 release];
+    [tc6 release];
+    
+    tv1 = [[NSTextView alloc] initWithFrame: NSMakeRect(0,	151,	150,150) textContainer: tc1];		
+    tv2 = [[NSTextView alloc] initWithFrame: NSMakeRect(151,	151,	150,150) textContainer: tc2];		
+    tv3 = [[NSTextView alloc] initWithFrame: NSMakeRect(0,	0,	150,150) textContainer: tc3];
+    tv4 = [[NSTextView alloc] initWithFrame: NSMakeRect(151,	0,	150,150) textContainer: tc4];
+    tv5 = [[NSTextView alloc] initWithFrame: NSMakeRect(360,	0,	150,300) textContainer: tc5];
+    tv6 = [[NSTextView alloc] initWithFrame: NSMakeRect(511,	0,	150,300) textContainer: tc6];
+    
+    [container addSubview: tv1];
+    [container addSubview: tv2];
+    [container addSubview: tv3];
+    [container addSubview: tv4];	
+    [container addSubview: tv5];
+    [container addSubview: tv6];
+    [tv1 release]; 
+    [tv2 release];
+    [tv3 release];
+    [tv4 release];
+    [tv5 release];
+    [tv6 release];
+
+    [[storage mutableString] appendString: @"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."];
+  }
   
-  [scrollView setDocumentView: textView];
-  RELEASE(textView);
-  
-  // TODO: Substitute the following with a Hbox with commands etc. 
-  externalBox = [NSBox new];
-  [externalBox setTitlePosition: NSNoTitle];
-  [externalBox setBorderType: NSNoBorder];
-  [externalBox addSubview: scrollView];
-  RELEASE (scrollView);
-  [externalBox sizeToFit];
-  [externalBox setAutoresizingMask: (NSViewWidthSizable 
-				     | NSViewHeightSizable)];
-  
-  winFrame.size = [externalBox frame].size;
+  winFrame.size = NSMakeSize(662,401);
   winFrame.origin = NSMakePoint (100, 200);
-  
+
+  label = [[NSTextField alloc] initWithFrame: NSMakeRect(0, 301, 662, 100)];
+  [label setStringValue: @"The four text views on the left share a layout manager, as do the two columns on the right. All six text views share the same underlying text storage."];
+  [label setBezeled: NO];
+  [label setDrawsBackground: NO];
+  [label setEditable: NO];
+  [container addSubview: label];
+  RELEASE (label);
+
   win = [[NSWindow alloc] initWithContentRect: winFrame
 			  styleMask: (NSTitledWindowMask 
 				      | NSClosableWindowMask 
@@ -87,8 +118,8 @@
 			  backing: NSBackingStoreBuffered
 			  defer: NO];
   [win setReleasedWhenClosed: NO];  
-  [win setContentView: externalBox];
-  RELEASE (externalBox);
+  [win setContentView: container];
+  RELEASE (container);
   [win setTitle: @"NSTextView Test"];
   
   [self restart];
@@ -97,6 +128,7 @@
 -(void) dealloc
 {
   RELEASE (win);
+  RELEASE (storage);
   [super dealloc];
 }
 -(void) restart
