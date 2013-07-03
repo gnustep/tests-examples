d1497541a39a982ff9ca41a78bd5f5986330ab20
Ivan Vučica <ivan@vucica.net>
2013-07-03 16:12:49 +0000

    MyTransparentGL: A demo that demonstrates how to create a transparent NSOpenGLView.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@36822 72102866-910b-0410-8b05-ffd578937521

---
+++ gui/MyTransparentGL/AppController.h
@@ -0,0 +1,10 @@
+/* All Rights reserved */
+
+#include <AppKit/AppKit.h>
+
+@interface AppController : NSObject
+{
+  id window;
+  NSMutableArray *glViews;
+}
+@end
---
+++ gui/MyTransparentGL/main.m
@@ -0,0 +1,7 @@
+#include <AppKit/AppKit.h>
+
+int main(int argc, const char **argv)
+{
+	NSApplicationMain(argc, argv);
+	return 0;
+}
