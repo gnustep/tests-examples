c40f7ab9664e7078d77cd1a2ad037acad152ada7
Fred Kiefer <fredkiefer@gmx.de>
2013-08-13 21:38:30 +0000

    Apply patch by Johannes Lundberg <johannes@brilliantservice.co.jp>, plus some coding style fixes.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/examples/trunk@36970 72102866-910b-0410-8b05-ffd578937521

--- gui/MyTransparentGL/AppController.m
+++ gui/MyTransparentGL/AppController.m
@@ -1,6 +1,5 @@
 /* All Rights reserved */
 
-#include "TransparentOpenGLView.h"
 #include <AppKit/AppKit.h>
 #include <AppKit/NSOpenGLView.h>
 #include "AppController.h"
@@ -56,7 +55,6 @@ static float DotProduct (Vector *v1, Vector *v2)
   return v1->X * v2->X + v1->Y * v2->Y + v1->Z * v2->Z;
 }
 
-
 static float Magnitude (Vector *v)
 {
   return sqrtf(v->X * v->X + v->Y * v->Y + v->Z * v->Z);
@@ -80,44 +78,54 @@ static void Normalize (Vector *v)
   Vector light;
   Vector vv;
   float xx;
+
   light.X = 1.0f + rtx;
   light.Y = 1.0f + rty;
   light.Z = 1.0f + rtz;;
   Normalize(&light);
 
-  for(t = 0; t < pcount; t++)
-  {
-    glBegin( GL_TRIANGLES );
+  for (t = 0; t < pcount; t++)
+    {
+      glBegin( GL_TRIANGLES );
 
-    vv = nvertices[pnormals[t].p1];
-    Normalize(&vv);
-    xx = DotProduct(&light,&vv);
-    if (xx < 0.0f) xx = 0;
-    glColor3f(xx,xx,xx);
-    glVertex3f(vertices[polygons[t].p1].X,
-        vertices[polygons[t].p1].Y,
-        vertices[polygons[t].p1].Z);
+      vv = nvertices[pnormals[t].p1];
+      Normalize(&vv);
+      xx = DotProduct(&light,&vv);
+      if (xx < 0.0f)
+        {
+          xx = 0;
+        }
+      glColor3f(xx,xx,xx);
+      glVertex3f(vertices[polygons[t].p1].X,
+                 vertices[polygons[t].p1].Y,
+                 vertices[polygons[t].p1].Z);
 
-    vv = nvertices[pnormals[t].p2];
-    Normalize(&vv);
-    xx = DotProduct(&light,&vv);
-    if (xx < 0.0f) xx = 0;
-    glColor3f(xx,xx,xx);
-    glVertex3f(vertices[polygons[t].p2].X,
-        vertices[polygons[t].p2].Y,
-        vertices[polygons[t].p2].Z);
-
-    vv = nvertices[pnormals[t].p3];
-    Normalize(&vv);
-    xx = DotProduct(&light,&vv);
-    if (xx < 0.0f) xx = 0;
-    glColor3f(xx,xx,xx);
-    glVertex3f(vertices[polygons[t].p3].X,
-        vertices[polygons[t].p3].Y,
-        vertices[polygons[t].p3].Z);
-
-    glEnd( );
-  }
+      vv = nvertices[pnormals[t].p2];
+      Normalize(&vv);
+      xx = DotProduct(&light,&vv);
+      if (xx < 0.0f)
+        {
+          xx = 0;
+        }
+      glColor3f(xx,xx,xx);
+      glVertex3f(vertices[polygons[t].p2].X,
+                 vertices[polygons[t].p2].Y,
+                 vertices[polygons[t].p2].Z);
+      
+      vv = nvertices[pnormals[t].p3];
+      Normalize(&vv);
+      xx = DotProduct(&light,&vv);
+      if (xx < 0.0f)
+        {
+          xx = 0;
+        }
+      glColor3f(xx,xx,xx);
+      glVertex3f(vertices[polygons[t].p3].X,
+                 vertices[polygons[t].p3].Y,
+                 vertices[polygons[t].p3].Z);
+      
+      glEnd();
+    }
 }
 
 - (void) draw
@@ -134,19 +142,19 @@ static void Normalize (Vector *v)
   */
   glColor3f(0.8f,0.5f,0.3f);
   glBegin( GL_TRIANGLES );
-  for(t = 0; t < pcount; t++)
-  {
-    glVertex3f(vertices[polygons[t].p1].X,
-        vertices[polygons[t].p1].Y,
-        vertices[polygons[t].p1].Z);
-    glVertex3f(vertices[polygons[t].p2].X,
-        vertices[polygons[t].p2].Y,
-        vertices[polygons[t].p2].Z);
-    glVertex3f(vertices[polygons[t].p3].X,
-        vertices[polygons[t].p3].Y,
-        vertices[polygons[t].p3].Z);
-  }
-  glEnd( );
+  for (t = 0; t < pcount; t++)
+    {
+      glVertex3f(vertices[polygons[t].p1].X,
+                 vertices[polygons[t].p1].Y,
+                 vertices[polygons[t].p1].Z);
+      glVertex3f(vertices[polygons[t].p2].X,
+                 vertices[polygons[t].p2].Y,
+                 vertices[polygons[t].p2].Z);
+      glVertex3f(vertices[polygons[t].p3].X,
+                 vertices[polygons[t].p3].Y,
+                 vertices[polygons[t].p3].Z);
+    }
+  glEnd();
 }
 
 @end
@@ -164,13 +172,19 @@ static void Normalize (Vector *v)
 @implementation MySAX
 - (id) init
 {
+  self = [super init];
+  if (self == nil)
+    {
+      return nil;
+    }
+
   idx = 0;
   mesh = [Mesh new];
   trackMeshVertices = NO;
   trackNormalVertices = NO;
   trackPolygons = NO;
     
-  return [super init];
+  return self;
 }
 
 - (Mesh *) currentMesh
@@ -182,99 +196,105 @@ static void Normalize (Vector *v)
 {
   int p;
   NSEnumerator *en;
+
   if (trackMeshVertices)
-  {
-          NSArray *ar, *parray;
-    str = [[str stringByTrimmingSpaces]
-      stringByReplacingString:@"\t"
-             withString:@""];
-    parray = [str componentsSeparatedByString:@"\n"];
-    mesh->vcount = [parray count];
-    mesh->vertices = malloc(sizeof(Vector) * mesh->vcount);
-
-    NSLog(@"import %d vertices",mesh->vcount);
-
-    p = 0;
-    en = [parray objectEnumerator];
-    while ((str = [en nextObject]))
     {
-      ar = [str componentsSeparatedByString:@" "];
-      if ([ar count] == 3)
-      {
-        mesh->vertices[p].X = [[ar objectAtIndex:0] floatValue];
-        mesh->vertices[p].Y = [[ar objectAtIndex:1] floatValue];
-        mesh->vertices[p].Z = [[ar objectAtIndex:2] floatValue];
-        p++;
-      }
+      NSArray *ar, *parray;
+
+      str = [[str stringByTrimmingSpaces]
+              stringByReplacingString:@"\t"
+                           withString:@""];
+      parray = [str componentsSeparatedByString:@"\n"];
+      mesh->vcount = [parray count];
+      mesh->vertices = malloc(sizeof(Vector) * mesh->vcount);
+      
+      NSLog(@"import %d vertices",mesh->vcount);
+      
+      p = 0;
+      en = [parray objectEnumerator];
+      while ((str = [en nextObject]))
+        {
+          ar = [str componentsSeparatedByString:@" "];
+          if ([ar count] == 3)
+            {
+              mesh->vertices[p].X = [[ar objectAtIndex:0] floatValue];
+              mesh->vertices[p].Y = [[ar objectAtIndex:1] floatValue];
+              mesh->vertices[p].Z = [[ar objectAtIndex:2] floatValue];
+              p++;
+            }
+        }
     }
-  }
   else if (trackNormalVertices)
-  {
-          NSArray *ar, *parray;
-    str = [[str stringByTrimmingSpaces]
-      stringByReplacingString:@"\t"
-             withString:@""];
-    parray = [str componentsSeparatedByString:@"\n"];
-    mesh->nvcount = [parray count];
-    mesh->nvertices = malloc(sizeof(Vector) * mesh->nvcount);
-
-    NSLog(@"import %d normal vertices",mesh->nvcount);
-
-    p = 0;
-    en = [parray objectEnumerator];
-    while ((str = [en nextObject]))
     {
-      ar = [str componentsSeparatedByString:@" "];
-      if ([ar count] == 3)
-      {
-        mesh->nvertices[p].X = [[ar objectAtIndex:0] floatValue];
-        mesh->nvertices[p].Y = [[ar objectAtIndex:1] floatValue];
-        mesh->nvertices[p].Z = [[ar objectAtIndex:2] floatValue];
-        p++;
-      }
-    }
-  }
-  else if (trackPolygons)
-  {
-    NSArray *parray = [str componentsSeparatedByString:@" "];
-    mesh->polygons[idx].p1 = [[parray objectAtIndex:0] intValue];
-    mesh->polygons[idx].p2 = [[parray objectAtIndex:2] intValue];
-    mesh->polygons[idx].p3 = [[parray objectAtIndex:4] intValue];
+      NSArray *ar, *parray;
 
-    mesh->pnormals[idx].p1 = [[parray objectAtIndex:1] intValue];
-    mesh->pnormals[idx].p2 = [[parray objectAtIndex:3] intValue];
-    mesh->pnormals[idx].p3 = [[parray objectAtIndex:5] intValue];
-    idx ++;
-  }
+      str = [[str stringByTrimmingSpaces]
+              stringByReplacingString:@"\t"
+                           withString:@""];
+      parray = [str componentsSeparatedByString:@"\n"];
+      mesh->nvcount = [parray count];
+      mesh->nvertices = malloc(sizeof(Vector) * mesh->nvcount);
+      
+      NSLog(@"import %d normal vertices",mesh->nvcount);
+      
+      p = 0;
+      en = [parray objectEnumerator];
+      while ((str = [en nextObject]))
+        {
+          ar = [str componentsSeparatedByString:@" "];
+          if ([ar count] == 3)
+            {
+              mesh->nvertices[p].X = [[ar objectAtIndex:0] floatValue];
+              mesh->nvertices[p].Y = [[ar objectAtIndex:1] floatValue];
+              mesh->nvertices[p].Z = [[ar objectAtIndex:2] floatValue];
+              p++;
+            }
+        }
+    }
+  else if (trackPolygons)
+    {
+      NSArray *parray = [str componentsSeparatedByString:@" "];
+
+      mesh->polygons[idx].p1 = [[parray objectAtIndex:0] intValue];
+      mesh->polygons[idx].p2 = [[parray objectAtIndex:2] intValue];
+      mesh->polygons[idx].p3 = [[parray objectAtIndex:4] intValue];
+      
+      mesh->pnormals[idx].p1 = [[parray objectAtIndex:1] intValue];
+      mesh->pnormals[idx].p2 = [[parray objectAtIndex:3] intValue];
+      mesh->pnormals[idx].p3 = [[parray objectAtIndex:5] intValue];
+      idx ++;
+    }
 }
 
 - (void) startElement: (NSString *)elementName
-       attributes: (NSMutableDictionary *)elementAttributes
+           attributes: (NSMutableDictionary *)elementAttributes
 {
-  if ([elementName isEqualToString:@"float_array"])
-  {
-    NSString *sourceName = [elementAttributes objectForKey:@"name"];
-    if ([sourceName isEqualToString:@"Sphere-Pos-Array"])
+  if ([elementName isEqualToString: @"float_array"])
     {
-      trackMeshVertices = YES;
-      trackNormalVertices = NO;
-      trackPolygons = NO;
-    }
-    else if ([sourceName isEqualToString:@"Sphere-Normal-Array"])
-    {
-      trackMeshVertices = NO;
-      trackNormalVertices = YES;
-      trackPolygons = NO;
-    }
-  }
+      NSString *sourceName = [elementAttributes objectForKey: @"name"];
 
+      if ([sourceName isEqualToString: @"Sphere-Pos-Array"])
+        {
+          trackMeshVertices = YES;
+          trackNormalVertices = NO;
+          trackPolygons = NO;
+        }
+      else if ([sourceName isEqualToString: @"Sphere-Normal-Array"])
+        {
+          trackMeshVertices = NO;
+          trackNormalVertices = YES;
+          trackPolygons = NO;
+        }
+    }
+  
   if ([elementName isEqualToString:@"polygons"])
-  {
-    int pc = [[elementAttributes objectForKey:@"count"] intValue];
-    mesh->polygons = malloc(sizeof(Tri) * pc);
-    mesh->pnormals = malloc(sizeof(Tri) * pc);
-    mesh->pcount = pc;
-  }
+    {
+      int pc = [[elementAttributes objectForKey: @"count"] intValue];
+
+      mesh->polygons = malloc(sizeof(Tri) * pc);
+      mesh->pnormals = malloc(sizeof(Tri) * pc);
+      mesh->pcount = pc;
+    }
 
   /*
   if ([elementName isEqualToString:@"polygons"])
@@ -288,12 +308,12 @@ static void Normalize (Vector *v)
     }
   }
   */
-  if ([elementName isEqualToString:@"p"])
-  {
+  if ([elementName isEqualToString: @"p"])
+    {
       trackMeshVertices = NO;
       trackNormalVertices = NO;
       trackPolygons = YES;
-  }
+    }
 }
 
 - (void) endElement:(NSString*)elementName
@@ -383,6 +403,7 @@ static void Normalize (Vector *v)
 - (void) drawRect:(NSRect)r
 {
   unsigned int i;
+ 
   glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
 
   glLoadIdentity();
@@ -393,32 +414,34 @@ static void Normalize (Vector *v)
   glRotatef(_rtz,0.0f,0.0f,1.0f);
 
   for (i=0; i<[meshArray count]; i++)
-  {
-    [[meshArray objectAtIndex: i] drawCelShadeWithRtx: _rtx
-                                                  rty: _rty
-                                                  rtz: _rtz];
-  }
+    {
+      [[meshArray objectAtIndex: i] drawCelShadeWithRtx: _rtx
+                                                    rty: _rty
+                                                    rtz: _rtz];
+    }
 
   [[self openGLContext] flushBuffer];
 }
 
 -(void) refresh
 {
-  [self setNeedsDisplay:YES];
+  [self setNeedsDisplay: YES];
 }
 
 - (void) loadModel
 {
   GSXMLParser *p;
   MySAX *h;
-  h = [MySAX new];
-  p = [GSXMLParser parserWithSAXHandler:h
-             withContentsOfFile:@"girl.dae"];
-  if ([p parse])
-  {
-    [meshArray addObject:[h currentMesh]];
-  }
 
+  h = [MySAX new];
+  p = [GSXMLParser parserWithSAXHandler: h
+            withContentsOfFile: [[NSBundle mainBundle] pathForResource: @"girl"
+                                                                ofType: @"dae"]];
+  if ([p parse])
+    {
+      [meshArray addObject: [h currentMesh]];
+    }
+  RELEASE(h);
 }
 
 @end
@@ -427,6 +450,7 @@ static void Normalize (Vector *v)
 {
 }
 @end
+
 @implementation MyTransparentGLView
 
 + (NSOpenGLPixelFormat*)defaultPixelFormat
@@ -443,7 +467,6 @@ static void Normalize (Vector *v)
   return [[NSOpenGLPixelFormat alloc] initWithAttributes: pf];
 }
 
-
 @end
 
 @implementation AppController
@@ -451,15 +474,16 @@ static void Normalize (Vector *v)
 -(void) applicationDidFinishLaunching: (NSNotification *)not
 {
   int i;
-  for (i=0; i<[glViews count]; i++)
-  {
-    [NSTimer scheduledTimerWithTimeInterval:0.05 //(1.0 / 30)
-      target:[glViews objectAtIndex: i]
-      selector:@selector(refresh)
-      userInfo:nil
-      repeats:YES];
-    [[glViews objectAtIndex: i] loadModel];
-  }
+
+  for (i = 0; i < [glViews count]; i++)
+    {
+      [NSTimer scheduledTimerWithTimeInterval: 0.05 //(1.0 / 30)
+                                       target: [glViews objectAtIndex: i]
+                                     selector: @selector(refresh)
+                                     userInfo: nil
+                                      repeats: YES];
+      [[glViews objectAtIndex: i] loadModel];
+    }
 }
 
 -(void) applicationWillFinishLaunching: (NSNotification *)not
@@ -474,6 +498,11 @@ static void Normalize (Vector *v)
   [glViews addObject: glview1];
   [glViews addObject: glview2];
   [glViews addObject: glview3];
+  /*
+  RELEASE(glview1);
+  RELEASE(glview2);
+  RELEASE(glview3);
+  */
 
   splitView1 = [[[NSSplitView alloc] initWithFrame: [window frame]] autorelease];
   [splitView1 addSubview: glview1];  
@@ -483,8 +512,10 @@ static void Normalize (Vector *v)
   [splitView2 addSubview: splitView1];
   [splitView2 addSubview: glview3];
   [splitView2 setVertical: YES];
+  //RELEASE(splitView1);
   
   [window setContentView: splitView2];
+  //RELEASE(splitView2);
 }
 
 @end
